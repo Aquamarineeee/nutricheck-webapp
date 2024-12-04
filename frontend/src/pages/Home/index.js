e508fe9e4f77c8f58ef5aa23c39140e90c23ad22
Aquamarineeee <emerldforever1609@gmail.com>
2024-12-04 23:30:19 +0700

    suadoi

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -3,20 +3,32 @@ import { useNavigate } from "react-router-dom";
 import About from "./About";
 import Services from "./Services";
 import styles from "../../styles/home.module.css";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const navigate = useNavigate();
+  const MyComponent = () => {
+    const [isMobile, setIsMobile] = useState(false);
+  
+    useEffect(() => {
+      const handleResize = () => setIsMobile(window.innerWidth <= 768);
+      handleResize(); // Kiểm tra ngay khi render lần đầu
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }, []);
   return (
     <div>
       <div className={styles.heroSection}>
         <img className={styles.logo} src="/static/img/logo.png" alt="Logo" />
         <div className={styles.titleWrapper}>
-          <img
-            className={styles.title}
-            src="/static/img/brandname.png"
-            alt="Brandname"
-            style={{ width: "380px", height: "auto" }}
-          />
+        <img
+      src="/static/img/brandname.png"
+      alt="Brandname"
+      style={{
+        width: isMobile ? "300px" : "500px",
+        height: "auto",
+      }}
+      />
           <p className={styles.subtitle}>
             Trợ lý dinh dưỡng để kiểm soát chế độ ăn uống, quản lý thói quen ăn
             uống và duy trì sức khỏe tốt. 
@@ -46,4 +58,5 @@ export default function Home() {
       <Services />
     </div>
   );
+}
 }
