38481aa5316ec254c414357a08f5cec3a9917018
Aquamarineeee <emerldforever1609@gmail.com>
2025-01-12 17:00:05 +0700

    t

--- frontend/src/Components/UserInfo.js
+++ frontend/src/Components/UserInfo.js
@@ -3,6 +3,9 @@ import { Alert, Typography } from "@mui/material";
 import Chart from "react-apexcharts";
 import { useSnackbar } from "notistack";
 import { AppContext } from "../Context/AppContext";
+import { totalNutrition } from "../services/apis";
+import { Button } from "@mui/material";
+
 
 const UserInfo = () => {
   const { enqueueSnackbar } = useSnackbar();
